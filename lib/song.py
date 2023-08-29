074c6da006edc6752fb3ca601f232ed64aee53bc
elsawanjiru <elsa.wanjiru@student.moringaschool.com>
2023-08-29 21:33:47 +0300

    Completed code challenge

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,42 @@
-from config import CONN, CURSOR
+import sqlite3
 
 class Song:
-    pass
+    CONN = None
+    CURSOR = None
+
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        cls.CONN = sqlite3.connect('db/music.db')  
+        cls.CURSOR = cls.CONN.cursor()
+
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+        cls.CURSOR.execute(sql)
+        cls.CONN.commit()
+
+    @classmethod
+    def close_connection(cls):
+        if cls.CONN:
+            cls.CONN.close()
+
+    def save(self):
+        if not self.CONN or not self.CURSOR:
+            raise ValueError("Database connection is not established. Call create_table() first.")
+
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+        self.CURSOR.execute(sql, (self.name, self.album))
+        self.id = self.CURSOR.lastrowid
+        self.CONN.commit()
